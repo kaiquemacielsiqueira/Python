b5732231e46750817a098ed9e7dc2461a314ef75
Kaique Siqueira <kaiquemaciels1@hotmail.com>
2024-11-29 08:55:02 -0300

    testes em ola mundo

---
+++ OlaMundo.py
@@ -0,0 +1,18 @@
+OlaMundo = 'Olá ' + 'Mundo'
+print(OlaMundo)
+
+p1 = 'Olá '
+p2 = 'Mundo'
+p3 = p1 + p2
+print(p3)
+
+print(3*p3)
+
+print(ord('o'))
+print(ord('l'))
+print(ord('a'))
+print(ord('m'))
+print(ord('u'))
+print(ord('n'))
+print(ord('d'))
+print(ord('o'))
